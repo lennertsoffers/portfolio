479548ce2b9bf36776bb2978ad340d4d805de80c
lennertsoffers <lennert.soffers@hotmail.com>
2023-01-31 08:39:15 +0100

    feat: setup project

---
+++ bundler/webpack.common.js
@@ -0,0 +1,100 @@
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCSSExtractPlugin = require('mini-css-extract-plugin');
+const path = require('path');
+
+module.exports = {
+    entry: path.resolve(__dirname, '../src/index.ts'),
+    module:
+    {
+        rules:
+            [
+                // HTML
+                {
+                    test: /\.(html)$/,
+                    use:
+                        [
+                            'html-loader'
+                        ]
+                },
+
+                // JS
+                {
+                    test: /\.js$/,
+                    exclude: /node_modules/,
+                    use:
+                        [
+                            'babel-loader'
+                        ]
+                },
+
+                {
+                    test: /\.ts$/,
+                    use: 'ts-loader',
+                    exclude: /node_modules/,
+                },
+
+                // CSS
+                {
+                    test: /\.css$/,
+                    use:
+                        [
+                            MiniCSSExtractPlugin.loader,
+                            'css-loader'
+                        ]
+                },
+
+                // Images
+                {
+                    test: /\.(jpg|png|gif|svg)$/,
+                    type: 'asset/resource',
+                    generator:
+                    {
+                        filename: 'assets/images/[hash][ext]'
+                    }
+                },
+
+                // Fonts
+                {
+                    test: /\.(ttf|eot|woff|woff2)$/,
+                    type: 'asset/resource',
+                    generator:
+                    {
+                        filename: 'assets/fonts/[hash][ext]'
+                    }
+                },
+
+                // Shaders
+                {
+                    test: /\.(glsl|vs|fs|vert|frag)$/,
+                    type: 'asset/source',
+                    generator:
+                    {
+                        filename: 'assets/images/[hash][ext]'
+                    }
+                }
+            ]
+    },
+    output:
+    {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    resolve: {
+        extensions: ['.ts', '.js'],
+    },
+    devtool: 'source-map',
+    plugins:
+        [
+            new CopyWebpackPlugin({
+                patterns: [
+                    { from: path.resolve(__dirname, '../static') }
+                ]
+            }),
+            new HtmlWebpackPlugin({
+                template: path.resolve(__dirname, '../src/index.html'),
+                minify: true
+            }),
+            new MiniCSSExtractPlugin()
+        ]
+};
